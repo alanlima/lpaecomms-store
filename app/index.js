26a5e100263cdb7f7375f8b4fc8148a34d202638
Alan Lima <alima@outlook.com>
2017-09-06 18:04:33 +1000

    project setup

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const HelloWorld = () => <h1>LPA eComms</h1>
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+//import path from 'path';
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+
+const config = {
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: "url-loader?limit=100000" },
+            // {
+            //     test: require.resolve('jquery'),
+            //     use: [{
+            //         loader: 'expose-loader',
+            //         options: 'jQuery'
+            //     },{
+            //         loader: 'expose-loader',
+            //         options: '$'
+            //     }]
+            // }
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        // new webpack.ProvidePlugin({
+        //     $: "jquery",
+        //     jQuery: "jquery"
+        // }),
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+        })
+    ],
+    resolve: {
+        modules: [
+            path.resolve('./app'),
+            path.resolve('./node_modules')
+        ]
+    }
+};
+
+config.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV) || JSON.stringify('dev')
+            }
+        })
+    );
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+    );
+}
+
+module.exports = config;
